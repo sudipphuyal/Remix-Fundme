ebb2fc2763fd079a72e6cc9e9c5f2267548a6bd8
sudipphuyal <sudip.phuyal@iscte-iul.pt>
2025-01-12 14:52:37 +0000

    FundMe initial file

--- FundMe.sol
+++ FundMe.sol
@@ -1,1 +1,17 @@
+// Get funds from users
+// withdraw funds
+//set a minimum funding in USD
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract FundMe{
+    function fund() public payable {}
+    // Allow users to send $
+    // Have a minimum $ sent
+    // 1. How do we send ETH to this contract?
+
+    
+
+
+    function withdraw() public {}
+}
