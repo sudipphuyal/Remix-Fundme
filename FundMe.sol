8c4267a957b46d3b5d2ab0675e5050c06cfb7651
sudipphuyal <sudip.phuyal@iscte-iul.pt>
2025-01-12 22:04:10 +0000

    incomplete fund me

--- FundMe.sol
+++ FundMe.sol
@@ -5,13 +5,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 contract FundMe{
-    function fund() public payable {}
+    uint256 public myValue =1;
+    function fund() public payable {
     // Allow users to send $
     // Have a minimum $ sent
     // 1. How do we send ETH to this contract?
+    require(msg.value>1e18,"didn't enough ETH"); //1e18 = 1 ETH = 1* 10**18 Wei
 
-    
+
+}
 
 
-    function withdraw() public {}
+    // function withdraw() public {}
+
+    function getPrice () public {
+        // Address
+        // ABI
+    }
 }
